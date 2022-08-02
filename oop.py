fa55340cba5b4c22eff4de00570efb42697cdd53
alfredoxavier <alfredoapxavier@gmail.com>
2022-08-02 20:55:33 -0300

    Terceiro commit

--- oop.py
+++ oop.py
@@ -1,7 +1,15 @@
 from triangulo import Triangulo
 
 a = Triangulo(3,4,5)
-print(a.area)
+print(f'O triangulo "a" possui {a.lados} lados, com os vertices a: {a.A}, b: {a.B} e c: {a.C}, possui área de {a.area} e perímetro de {a.perimetro}')
+print('Agora troque os vértices')
+a.troca_vertices()
+print(f'Agora, o triangulo a possui {a.lados} lados, com os vertices a: {a.A}, b: {a.B} e c: {a.C}, possui nova área de {a.area} e perímetro de {a.perimetro} ')
+
+
+
+
+
 
 # Triangulo.A = float(input('Entre com o lado A: '))
 # Triangulo.B = float(input('Entre com o lado B: '))
--- triangulo.py
+++ triangulo.py
@@ -7,14 +7,33 @@ class Triangulo(object):
         self.B = b
         self.C = c
         self.area = float()
+        self.perimetro = float()
         self.calc_area()
-    
+        self.calc_perimetro()
+          
     def troca_vertices(self):
-      self.A = float(input('Vertice A: '))
-      self.B = float(input('Vertice B: '))
-      self.C = float(input('Vertice C: '))
+      try:
+        self.A = float(input('Vertice A: '))
+        self.B = float(input('Vertice B: '))
+        self.C = float(input('Vertice C: '))
+        self.calc_area()
+        self.calc_perimetro()
+      
+      except(ValueError):
+        print('Os lados dos triangulo devem ser numeros!')
+        ans = (input('Deseja tentar novamente (S/N)'))
+        ans = ans.upper()
+        if ans == ('S'):
+          self.troca_vertices() 
+        elif ans == ('N'):
+          print('Cancelado pelo usuário')
+        else:
+          print('Resposta incorreta')
 
     def calc_area(self):
         p = (self.A + self.B + self.C) / 2
         self.area = math.sqrt(p*(p-self.A)*(p-self.B)*(p-self.C))
         # return Area, p
+
+    def calc_perimetro(self):
+      self.perimetro = self.A + self.B + self.C
